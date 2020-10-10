da6024be5f18c1cc885fb66155acb81fc1361753
Yusuf Wiryonoputro <yusw@Yusufs-MacBook-Air.local>
2020-10-10 21:01:58 +0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://d4lkkfqr81.execute-api.us-west-2.amazonaws.com/production"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_lDNogReGl",
+        APP_CLIENT_ID: "14m4oelnp58si7g74kdrhv58h0",
+        IDENTITY_POOL_ID: "us-west-2:ed4aec0e-1e48-4c04-b54a-920caa5dca0a"
+    }
+};
